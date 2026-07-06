4417b23d4941431ddfac9ef0b8f5ae460a9f9192
Vismay Shah <vismay.shah@interfaceware.com>
2026-07-05 21:03:23 -0400

    Tone enabled button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import * as Tone from "tone"
 
+
+interface ToneButtonProps {
+   synth: Tone.Synth;
+}
+
+const ToneButton = ({synth} : ToneButtonProps) =>{
+   const [count, setCount] = useState(0);
+   
+   const handleClick = async() => {
+      await Tone.start();
+      synth.triggerAttackRelease("C4", "8n");
+      setCount(count + 1);
+   }
+
+   return (
+      <button className="bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 active:bg-blue-800 transition" 
+              onClick={handleClick}>
+      ♫ played count: {count}
+      </button>
+   )
+}
+
 function App() {
-  return (
-    <>
-      <main>
-        <h1 className='text-3xl font-bold text-black-500'>Press Me</h1>
+   // App-wide synth, only want 1
+   const synth = useMemo(() => new Tone.Synth().toDestination(), []);
+
+   return (
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+         <h1 className='text-3xl font-bold text-gray-800'>Press Me</h1>
+         <ToneButton synth={synth}/>
       </main>
-    </>
-  )
+   )
 }
 
 export default App

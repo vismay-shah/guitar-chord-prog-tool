4161614b6c44b6ac42f6a77c9cdc6a7f829ddc98
Vismay Shah <vismay.shah@interfaceware.com>
2026-07-05 20:06:34 -0400

    Blank slate with Tone imported

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import * as Tone from "tone"
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1 className='text-3xl font-bold text-black-500'>Press Me</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
